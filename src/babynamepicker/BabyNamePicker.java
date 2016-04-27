d0c1eaa95ef73dc5a74fd9a991bb8ac1079590aa
leeji14 <glissful@gmail.com>
2016-04-27 17:38:51 -0400

    added list to panel

--- src/babynamepicker/BabyNamePicker.java
+++ src/babynamepicker/BabyNamePicker.java
@@ -1,6 +1,8 @@
 package babynamepicker;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -9,13 +11,16 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 
 /**
@@ -45,6 +50,11 @@ public class BabyNamePicker {
 	private String[] numSuggestions;
 	private JButton readyToRankButton;
 	private JList namesList;
+	private JScrollPane listScroller;
+	private DefaultListModel listModel;
+	private String[] testNames; //For GUI testing, don't actually use this in final
+	private String[] secondTestNames = new String[] {"Harry", "Hermione", "Ron", "Snape", "Dumbledore", "Remus", "Fred", "George"};
+	
 	
 	/** The Model is the object that does all the computations. It is
      * completely independent of the Controller and View objects. */
@@ -100,7 +110,7 @@ public class BabyNamePicker {
 		
 //		leftPanel = new JPanel(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
 		leftPanel = new JPanel(new GridBagLayout());
-		rightPanel = new JPanel(); //Choose layout
+		rightPanel = new JPanel(new BorderLayout()); //Choose layout
 		
 		genderText = new JTextArea("Gender of the Baby: ");
 		popularText = new JTextArea("Sort by Commonality: ");
@@ -125,7 +135,19 @@ public class BabyNamePicker {
 		numSuggestions = new String[] {"10", "20", "30","40", "50"};
 		numSuggestionsMenu = new JComboBox<String>(numSuggestions);
 		
-//		namesList = new JList(BabyName);
+		listModel = new DefaultListModel();
+		testNames = new String[] {"Grace", "Vicky", "Ryan", "Gabe", "Lydia", "Danny", "Matthew", "Ashley", "Harry", "Hermione", "Ron", "Snape", "Dumbledore", "Remus", "Fred", "George"};
+		for (int i = 0; i < testNames.length; i++) {
+			listModel.addElement(testNames[i]);
+		}
+		namesList = new JList(listModel);
+		namesList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		namesList.setLayoutOrientation(JList.VERTICAL);
+		namesList.setVisibleRowCount(10);
+		listScroller = new JScrollPane(namesList);
+		listScroller.setPreferredSize(new Dimension(150, 400));
+		
+		
 		
 		frame.setLayout(new GridBagLayout()); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -174,12 +196,13 @@ public class BabyNamePicker {
 		c.gridy = 1;
 		frame.add(leftPanel, c);
 		view.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		c.gridx = 2;
+		c.gridx = 3;
 		c.gridy = 1;
 		c.weightx = 0.9;
 		c.anchor = GridBagConstraints.CENTER;
-		frame.add(view, c);
-		
+//		frame.add(view, c);
+		frame.add(rightPanel, c);
+		rightPanel.add(listScroller, BorderLayout.CENTER);
 		
 	}
 	
@@ -249,6 +272,30 @@ public class BabyNamePicker {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
+				JComboBox c = (JComboBox) e.getSource();
+				String selectedItem = (String) c.getSelectedItem();
+				int numSuggestions = Integer.parseInt(selectedItem);
+				
+				switch(numSuggestions) {
+				case 10:
+					System.out.println("10 users selected.");
+					break;
+				case 20:
+					System.out.println("20 users selected.");
+					listModel.remove(5);
+					listModel.remove(3);
+					break;
+				case 30:
+					System.out.println("30 users selected.");
+					break;
+				case 40:
+					System.out.println("40 users selected.");
+					break;
+				case 50:
+					System.out.println("50 users selected.");
+					break;
+				}
+				
 				
 			}
     		
@@ -258,7 +305,7 @@ public class BabyNamePicker {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				System.out.println("Button pressed");
+//				System.out.println("Button pressed");
 				
 			}
     		
